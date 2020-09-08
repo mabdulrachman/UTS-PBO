322de9972fc7d0ffb9297bcd1ca6cae55caddcf6
mabdulrachman <64099245+mabdulrachman@users.noreply.github.com>
2020-09-08 14:01:32 +0700

    Add files via upload

---
+++ mabdul.java
@@ -0,0 +1,17 @@
+package UTS;
+
+public class mabdul {
+
+	public static void main(String[] args) {
+		
+		String a = "Moch Abdul Rachman Hakim";
+		String b = new String ("XII RPL-B");
+		char dul[] = {'P','B','O'};
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(dul);
+
+	}
+
+}
